e3cd057bcf9c1edd889c1b7aec248076286b31c0
suresh-ayyannagari <ayyannagarisuresh@gmail.com>
2022-03-18 21:09:05 +0530

    first

--- src/com/simplilearn/array/Exercise1.java
+++ src/com/simplilearn/array/Exercise1.java
@@ -5,6 +5,11 @@ public class Exercise1 {
 	public static void main(String[] args) {
 		// WAP to create collection price array => float[]
 		// access data by inde x and iterate over all values.
+	float[] arr=new float[]{1.2f,3.4f};
+	for(int i=0;i<arr.length();i++){
+	System.out.println(arr[i]);
+	}
+
 
 	}
 
