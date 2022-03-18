aed730eadcbb65ed3d54c649e44f84c56c994fca
suresh-ayyannagari <101242366+suresh-ayyannagari@users.noreply.github.com>
2022-03-18 22:32:23 +0530

    Update Exercise1.java

--- src/com/simplilearn/typecasting/Exercise1.java
+++ src/com/simplilearn/typecasting/Exercise1.java
@@ -3,6 +3,19 @@ package com.simplilearn.typecasting;
 public class Exercise1 {
 
 	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("enter a integer:");
+		int number=input.nextInt();
+		float floatnum=number;
+		double doublenum=floatnum;
+		long longnum=number;
+
+		System.out.println("integer:"+number);
+		System.out.println("float:"+floatnum);
+		System.out.println("double:"+doublenum);
+		System.out.println("long:"+longnum);
+	
+
 		
 		// TODO :: WAP for data converting from int, byte, short  -> float , double & long.
 
